55311a606e6f9a59461e9c1ae0b7f4fe424e711f
Shahed Ahmad <shaheda.a16@gmail.com>
2021-02-12 19:51:15 +0430

    Hello world program created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/hello'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+        <Greet />
       </header>
     </div>
   );
---
+++ src/components/hello.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function hello() {
+    return (
+        <div>
+            Hello World!
+        </div>
+    )
+}
+
+export default hello
