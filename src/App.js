7618f3b35039fbd66c20ee2082bb702f14337d58
Shahed Ahmad <shaheda.a16@gmail.com>
2021-02-15 21:34:08 +0430

    Hooks started

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import { Writer } from './components/textAdder'
 import Counter from './components/inputCounter'
 import AddRemove from './components/AddRemove'
+import HookCounter from "./components/HooksFullCounter";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <AddRemove />
+        <HookCounter />
       </header>
     </div>
   );
