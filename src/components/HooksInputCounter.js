2a04fcc055eb1b7b519f4e45b9e23ea58b558381
Shahed Ahmad <shaheda.a16@gmail.com>
2021-02-16 13:25:06 +0430

    HookInputCounter upgraded and styled

--- src/components/HooksInputCounter.js
+++ src/components/HooksInputCounter.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function HooksInputCounter() {
+
+    let inputRef = React.createRef();
 
     const [val, setVal] = useState('');
     const [add, setAdd] = useState(0);
@@ -23,12 +25,25 @@ function HooksInputCounter() {
         
     }
 
+    let enter = (e) => {
+        console.log(e);
+        if (e.charCode === 13){
+            pass()
+        }else{
+            setVal(e.target.value)
+        }
+    }
+
+    useEffect(() => {
+        inputRef.current.focus()
+    }, [])
+
     return (
-        <div>
-            <h1>{add}</h1>
-            <input value={val} onChange={(e) => setVal(e.target.value)} className="text-black" />
-            <button onClick={pass}>Add</button>
-            <p className="">{show}</p>
+        <div className="bg-white rounded p-5">
+            <h1 className="text-black font-semibold">{add}</h1>
+            <input value={val} ref={inputRef} onChange={(e) => setVal(e.target.value)} onKeyPress={(e) => enter(e)} className="text-black border-2 my-2  rounded-l bg-gray-200 px-3 outline-none text-base py-1" />
+            <button onClick={pass} className="text-white font-semibold bg-purple-500 border-2 rounded-r px-4 text-base py-1">Add</button>
+            <p className="text-sm text-red-600">{show}</p>
         </div>
     )
 }
