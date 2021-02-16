c800c5604da690cbcb7625c298281ffe72fe96a4
Shahed Ahmad <shaheda.a16@gmail.com>
2021-02-16 14:07:25 +0430

    Debugged and fully functional

--- src/components/HookAddRemove.js
+++ src/components/HookAddRemove.js
@@ -3,19 +3,22 @@ import React, { useState, useEffect } from "react";
 function HookAddRemove() {
 
     const [val, setVal] = useState("");
-    const [show, setShow] = useState('')
-    const [update, setUpdate] = useState([])
+    const [update, setUpdate] = useState([]);
+    const [count, setCount] = useState(0)
 
     const inpt = () => {
-        update.push(val)
-        setVal('');
-        setShow('');        
+        update.push([val, count]);
+        console.log(val, count);
+        setVal('');   
+        setCount(prev => prev + 1);    
     }
 
     const remove = (in_arr_index) => {
-        update.filter((val, ind) => {return val[1] != in_arr_index})
+        let new_update = update.filter((val) => {console.log(val, in_arr_index)
+             return val[1] != in_arr_index});
+        
+        setUpdate(new_update);
     }
-
     return (
         <div>
             <input value={val} onChange={(e) => setVal(e.target.value)} className="text-black" />
@@ -24,8 +27,8 @@ function HookAddRemove() {
                 {update &&
                     update.map((item) => (
                         <div key={item[1]}>
-                            <span className="text-black ">{item[0]}</span>
-                            <button onClick={() => remove([item[1]])}>
+                            <span className="text-black">{item[0]}</span>
+                            <button onClick={() => remove(item[1])}>
                                 &times;
                             </button>
                         </div>
