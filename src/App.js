883abe43d91d2071a8650d0c0250bafa716ffe4a
Shahed Ahmad <shaheda.a16@gmail.com>
2021-11-11 22:42:33 +0430

    trying to user context api in class components and context created

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import { Writer } from './components/textAdder'
-import Counter from './components/inputCounter'
-import AddRemove from './components/AddRemove'
-import HookCounter from "./components/HooksFullCounter";
-import InputCounter from './components/HooksInputCounter';
+// import { Writer } from './components/textAdder'
+// import Counter from './components/inputCounter'
+// import AddRemove from './components/AddRemove'
+// import HookCounter from "./components/HooksFullCounter";
+// import InputCounter from './components/HooksInputCounter';
 import AddRemoveH from './components/HookAddRemove'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* <HookCounter /> */}
-        {/* <InputCounter /> */}
+        <HookCounter />
+        <InputCounter />
         <AddRemoveH />
-      </header>
+      </header> */}
+      
     </div>
   );
 }
